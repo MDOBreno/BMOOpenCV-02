51e5418dbf38d4b2560bdacc3412f68b49bc60aa
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-03-28 18:52:22 -0300

    Feito!

--- BMOOpenCV-02/main.cpp
+++ BMOOpenCV-02/main.cpp
@@ -6,10 +6,25 @@
 //  Copyright © 2020 ProgramasBMO. All rights reserved.
 //
 
-#include <iostream>
+//#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    String caminho = "/Users/brenomedeiros/Documents/ProgramasBMO/Cpp/BMOOpenCV-02/BMOOpenCV-02/";
+    String caminhoImagens = "/Users/brenomedeiros/Documents/ProgramasBMO/Cpp/BMOOpenCV-02/BMOOpenCV-02/Images/";
+    
+    Mat testeCor = imread(caminho+"Images/Me.jpeg", CV_LOAD_IMAGE_COLOR);
+    Mat testeCinza = imread(caminho+"Images/Me.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
+    
+    imshow("cor", testeCor);
+    imshow("cinza", testeCinza);
+    
+    imwrite(caminho+"Me-Cinza.jpeg", testeCinza);
+    
+    waitKey();
+    
     return 0;
 }
